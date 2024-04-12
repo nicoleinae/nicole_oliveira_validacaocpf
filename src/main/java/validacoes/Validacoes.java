3cd8e003ba99e0a1bada271e56d7cada27077207
nicoleinae <nicoleinae@gmail.com>
2024-04-12 15:12:09 -0300

    feat: criação do metodo removerpontosetracos dentro da classe validacoes.

--- src/main/java/validacoes/Validacoes.java
+++ src/main/java/validacoes/Validacoes.java
@@ -3,15 +3,30 @@ package validacoes;
 public class Validacoes {
 
     public boolean tamanho(String cpf) {
-        if (cpf.length() == 11) {
+        String cpfcopia = removerPontosTracos(cpf);
+        if (cpfcopia.length() == 11) {
             return true;
         }
         else {
             return false;
         }
     }
+<<<<<<< Updated upstream
     
+=======
+    public boolean numeros(String cpf) {
+        String cpfcopia = removerPontosTracos(cpf);
+        try {
+            Long.parseLong(cpfcopia);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+>>>>>>> Stashed changes
     }
 
+    public String removerPontosTracos(String cpf) {
+        return cpf.replaceAll("[.-]", "");
+    }
 
 }
