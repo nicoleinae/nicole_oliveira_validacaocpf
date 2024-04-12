a8c99d5503a96f1eb25e2513653615132452f0a6
nicoleinae <nicoleinae@gmail.com>
2024-04-12 08:45:27 -0300

    feat: criação da classe CPF e instanciação do objeto na lista de CPFs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import cpf.CPF;
+
 public class Main {
     public static void main(String[] args) {
+        List<CPF> listaCPF = new ArrayList<>();
 
-        System.out.println("Hello world!");
+        listaCPF.add(new CPF("98765432100"));
+        listaCPF.add(new CPF("11122233344"));
+        listaCPF.add(new CPF("12345678910"));
+        listaCPF.add(new CPF("55566677788"));
+        listaCPF.add(new CPF("12345678912312"));
+        listaCPF.add(new CPF("12312312"));
+        listaCPF.add(new CPF("12"));
+        listaCPF.add(new CPF("12345678944"));
+        listaCPF.add(new CPF("A12547"));
+        listaCPF.add(new CPF("A1231231232"));
+        listaCPF.add(new CPF("123.123.123-22"));
+        listaCPF.add(new CPF("123.123.123-23"));
+        listaCPF.add(new CPF("123.123.123-24"));
+        listaCPF.add(new CPF("123.123.123-241"));
+
+
     }
 }
---
+++ src/main/java/cpf/CPF.java
@@ -0,0 +1,18 @@
+package cpf;
+
+public class CPF {
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // Método construtor
+    public CPF(String cpf) {
+        this.cpf = cpf;
+    }
+}
